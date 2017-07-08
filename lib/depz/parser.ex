f46007a3ecc8167280b9c97817a4cf6ef991b90a
MainShayne233 <shaynetremblay@hotmail.com>
2017-07-08 17:46:08 -0400

    Create Parser.parse_deps function

--- lib/depz/parser.ex
+++ lib/depz/parser.ex
@@ -1,15 +1,26 @@
 defmodule Depz.Parser do
 
-  @deps_start_indicators [
-    "defp deps",
-  ]
+  @doc """
+  Returns a list of the deps in the mix.exs file
+  """
+  @spec parse_deps(String.t) :: {:ok, [{atom, [atom | String.t]}]} | {:error, String.t}
+  def parse_deps(file) do
+    with {:ok, deps_section} <- deps_section(file) do
+      deps =
+        ~r/\[(.|\n)*\]/
+        |> Regex.scan(deps_section)
+        |> Enum.at(0)
+        |> Enum.at(0)
+        |> Code.eval_string
+        |> elem(0)
 
-  @deps_end_indicators [
-    "end",
-  ]
+      {:ok, deps}
+    end
+  end
+
 
   @doc """
-  Should return the correct case for the given file
+  Returns the correct deps list case for the given file, if it can be parsed
   """
   @spec parse_case(String.t) :: {:ok, atom} | {:error, String.t}
   def parse_case(file) do
@@ -26,6 +37,30 @@ defmodule Depz.Parser do
     |> String.replace(" ", "")
     |> case do
       "[]" -> {:ok, :empty_list}
+      _other -> check_for_collapsed_list_case(file)
+    end
+  end
+
+
+  defp check_for_collapsed_list_case(file) do
+    file
+    |> String.split("\n")
+    |> Enum.at(1)
+    |> Kernel.=~(~r/\[\s*{.*}/)
+    |> case do
+      true -> {:ok, :collapsed_list}
+      false -> check_for_open_list_case(file)
+    end
+  end
+
+
+  defp check_for_open_list_case(file) do
+    file
+    |> String.split("\n")
+    |> Enum.at(1)
+    |> String.replace(" ", "")
+    |> case do
+      "[" -> {:ok, :open_list}
       _other -> {:error, "Could not identify case"}
     end
   end
@@ -45,15 +80,16 @@ defmodule Depz.Parser do
 
   defp deps_range(file) do
     lines = file |> String.split("\n")
-    with {:ok, start_index} <- find_matching_line(lines, @deps_start_indicators),
-         {:ok, end_index} <- find_matching_line(lines, @deps_end_indicators, start_index) do
+    with {:ok, start_index} <- find_matching_line(lines, deps_start_matchers()),
+         {:ok, end_index} <- find_matching_line(lines, deps_end_matcher(), start_index) do
 
       {:ok, start_index..end_index}
     end
   end
 
 
-  defp find_matching_line(lines, matchers, start_index \\ 0) do
+  defp find_matching_line(lines, matchers, start_index \\ 0)
+  defp find_matching_line(lines, matchers, start_index) when matchers |> is_list do
     lines
     |> Enum.slice(start_index..-1)
     |> Enum.find_index(fn line ->
@@ -67,4 +103,30 @@ defmodule Depz.Parser do
       index -> {:ok, index + start_index}
     end
   end
+
+
+  defp find_matching_line(lines, matcher, start_index) when matcher |> is_function do
+    lines
+    |> Enum.slice(start_index..-1)
+    |> Enum.find_index(fn line ->
+      matcher.(line)
+    end)
+    |> case do
+      nil -> {:error, "No line for matchers"}
+      index -> {:ok, index + start_index}
+    end
+  end
+
+  defp deps_start_matchers do
+    [
+      "defp deps",
+    ]
+  end
+
+  defp deps_end_matcher do
+    fn line ->
+      String.replace(line, " ", "") == "end"
+    end
+  end
+
 end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -2,6 +2,29 @@ defmodule Depz.ParserTest do
   use ExUnit.Case, async: true
   alias Depz.Parser
 
+
+  describe("parse_deps/1") do
+    test "should return a list of the deps" do
+      {:ok, deps} =
+        variety_of_deps_file()
+        |> Parser.parse_deps
+
+      assert deps == [
+        {:cowboy, "~> 1.0", optional: true},
+        {:plug, "~> 1.3.2 or ~> 1.4"},
+        {:phoenix_pubsub, "~> 1.0"},
+        {:poison, "~> 2.2 or ~> 3.0"},
+        {:gettext, "~> 0.8", only: :test},
+        {:ex_doc, "~> 0.16", only: :docs},
+        {:inch_ex, "~> 0.2", only: :docs},
+        {:phoenix_guides, git: "https://github.com/phoenixframework/phoenix_guides.git", compile: false, app: false, only: :docs},
+        {:phoenix_html, "~> 2.6", only: :test},
+        {:websocket_client, git: "https://github.com/jeremyong/websocket_client.git", only: :test},
+      ]
+    end
+  end
+
+
   describe "parse_case/1" do
     test "should identify the [] case" do
       {:ok, deps_case} =
@@ -11,6 +34,7 @@ defmodule Depz.ParserTest do
       assert deps_case == :empty_list
     end
 
+
     test "should identify the collapsed_list case" do
       {:ok, deps_case} =
         collapsed_list_case_file()
@@ -18,7 +42,57 @@ defmodule Depz.ParserTest do
 
       assert deps_case == :collapsed_list
     end
+
+
+    test "should identify the open_list case" do
+      {:ok, deps_case} =
+        open_list_case_file()
+        |> Parser.parse_case
+
+      assert deps_case == :open_list
+    end
   end
+
+
+  defp variety_of_deps_file do
+    """
+      defmodule Depz.Mixfile do
+        use Mix.Project
+
+        def project do
+          [app: :depz,
+           version: "0.1.0",
+           elixir: "~> 1.4",
+           build_embedded: Mix.env == :prod,
+           start_permanent: Mix.env == :prod,
+           deps: deps()]
+        end
+
+        def application do
+          [extra_applications: [:logger]]
+        end
+
+        defp deps do
+          [
+            {:cowboy, "~> 1.0", optional: true},
+            {:plug, "~> 1.3.2 or ~> 1.4"},
+            {:phoenix_pubsub, "~> 1.0"},
+            {:poison, "~> 2.2 or ~> 3.0"},
+            {:gettext, "~> 0.8", only: :test},
+
+            # Docs dependencies
+            {:ex_doc, "~> 0.16", only: :docs},
+            {:inch_ex, "~> 0.2", only: :docs},
+            {:phoenix_guides, git: "https://github.com/phoenixframework/phoenix_guides.git", compile: false, app: false, only: :docs},
+            # Test dependencies
+            {:phoenix_html, "~> 2.6", only: :test},
+            {:websocket_client, git: "https://github.com/jeremyong/websocket_client.git", only: :test}
+          ]
+        end
+      end
+    """
+  end
+
 
   defp empty_list_case_file do
     """
@@ -45,6 +119,7 @@ defmodule Depz.ParserTest do
     """
   end
 
+
   defp collapsed_list_case_file do
     """
       defmodule Depz.Mixfile do
@@ -67,6 +142,36 @@ defmodule Depz.ParserTest do
           [{:dep1, "~> 1.2"},
            {:dep2, "~> 1.4"},
            {:dep3, "~> 1.5"}]
+        end
+      end
+    """
+  end
+
+
+  defp open_list_case_file do
+    """
+      defmodule Depz.Mixfile do
+        use Mix.Project
+
+        def project do
+          [app: :depz,
+           version: "0.1.0",
+           elixir: "~> 1.4",
+           build_embedded: Mix.env == :prod,
+           start_permanent: Mix.env == :prod,
+           deps: deps()]
+        end
+
+        def application do
+          [extra_applications: [:logger]]
+        end
+
+        defp deps do
+          [
+            {:dep1, "~> 1.2"},
+            {:dep2, "~> 1.4"},
+            {:dep3, "~> 1.5"}
+          ]
         end
       end
     """
